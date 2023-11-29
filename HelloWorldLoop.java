99fa14f38255ad2ff0645715d651eaf984877a15
Tasari97 <tasari0957@students.mc3.edu>
2023-11-29 10:39:56 -0500

    Add HelloWorldLoop class
    Created a new Java program that prints hello world, loops and prints 1 through 100.

---
+++ HelloWorldLoop.java
@@ -0,0 +1,13 @@
+public class HelloWorldLoop
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Hello, World!");
+      
+      // Loop and print numbers from 1 to 100
+      for (int i = 1; i <= 100; i++) 
+      {
+         System.out.println(i);
+      }
+    }
+}
